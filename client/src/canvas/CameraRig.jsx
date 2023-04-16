88ced47052f1383a0cb896ded526cb3cc896ae9e
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-04-16 13:38:40 +0530

    Update CameraRig.jsx

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -14,7 +14,7 @@ const CameraRig = ({ children }) => {
     const isMobile = window.innerWidth <= 600;
 
     // set the initial position of the model
-    let targetPosition = [-0.4, 0, 2];
+    let targetPosition = [-0.2, 0, 2];
     if (snap.intro) {
       if (isBreakpoint) {
         targetPosition = [0, 0, 2];
